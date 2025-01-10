6a4f0e2913a12b018ae23bf89cb7cb62d5a9e3e5
Dobren Dragojević <100283771+ddobren@users.noreply.github.com>
2025-01-10 15:39:08 +0100

    Update SearchForm.tsx Token JWT

--- Frontend/src/components/Search/SearchForm.tsx
+++ Frontend/src/components/Search/SearchForm.tsx
@@ -39,7 +39,7 @@ export const SearchForm: React.FC<SearchFormProps> = ({ onSearchStart, onSearchC
           return;
         }
 
-        const token = 'YOUR_TOKEN';
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.4rz5eH4rojtUQSPI8CcroOf4CRJjo6N9_HQAI_9e1t0';
 
         const responseMiddle = await fetch('https://engine.eduformacije.com/api/v1/skole/srednje', {
           headers: {
