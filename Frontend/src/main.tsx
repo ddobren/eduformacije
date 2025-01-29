5ddf370e5273576282afe8c6cbbc4e41f3013fe6
Dobren Dragojević <dragojevicdobren@gmail.com>
2025-01-29 01:00:31 +0100

    Dodana Vercel analitika

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
-import { Analytics } from '@vercel/analytics/react'; // Samo ako koristimo Vercel servis
-import { SpeedInsights } from '@vercel/speed-insights/react'; // Samo ako koristimo Vercel servis
+import { Analytics } from "@vercel/analytics/react"; // Samo ako koristimo Vercel servis
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Samo ako koristimo Vercel servis
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
