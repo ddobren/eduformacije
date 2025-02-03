22ce4019b709c96fcd1e61cf3dbf171f0a9e001b
Dobren Dragojević <dragojevicdobren@gmail.com>
2025-02-04 00:05:06 +0100

    Poboljsan embed sys

--- Backend/engine/handlers/serveStaticSkole.go
+++ Backend/engine/handlers/serveStaticSkole.go
@@ -1,28 +1,26 @@
 package handlers
 
 import (
+	_ "embed"
 	"encoding/json"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed srednje-programi.json
+var srednjeProgrami []byte
+
+// ServeStaticSkoleHandler vraća JSON podatke iz ugrađene datoteke
 func ServeStaticSkoleHandler(c *gin.Context) {
-	filePath := "../data/srednje-programi.json"
-
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Neuspjelo učitavanje JSON datoteke"})
-		return
-	}
-
-	// Parsiraj JSON da provjerimo ispravnost
 	var jsonData interface{}
-	if err := json.Unmarshal(data, &jsonData); err != nil {
+
+	// Parsiranje JSON-a kako bi se osiguralo da je ispravan
+	if err := json.Unmarshal(srednjeProgrami, &jsonData); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Neispravan JSON format"})
 		return
 	}
 
-	c.Data(http.StatusOK, "application/json", data)
+	// Pošalji JSON kao HTTP odgovor
+	c.Data(http.StatusOK, "application/json", srednjeProgrami)
 }
